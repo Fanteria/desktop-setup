719b7d1e4bd8c34bd05291243b01302888bcf70b
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2023-02-11 14:43:35 +0100

    Update i3 configuration.

--- config/i3/set_layout.sh
+++ config/i3/set_layout.sh
@@ -3,11 +3,11 @@
 sleep 5
 i3-msg "workspace 9: ; append_layout ~/.config/i3/ws.json"
 (kitty btop &)
-sleep 0.2
-(kitty /home/jirka/Projects/gcaltasks/main.py cal show &)
-sleep 0.2
-(kitty /home/jirka/Projects/gcaltasks/main.py task show &)
-sleep 0.2
+sleep 0.5
+(kitty /home/jirka/Projects/gcaltasks/gcaltasks/main.py cal show &)
+sleep 0.5
+(kitty /home/jirka/Projects/gcaltasks/gcaltasks/main.py task show &)
+sleep 0.5
 (kitty spt &)
 
 # i3-save-tree --workspace 9 > ~/ws.json && sed -i 's|^\(\s*\)// "|\1"|g; /^\s*\/\//d' ~/ws.json
