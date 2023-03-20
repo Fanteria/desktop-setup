f0e9db4ebc93d0cf4457f5771d2aa92ae0ca551e
Jiří Alexandrovič <jiri.alexandrovic@aveco.com>
2023-03-20 13:36:44 +0100

    Finally fix init script to be little bit robust.

--- init_script.sh
+++ init_script.sh
@@ -4,12 +4,21 @@ if [ -z "${HOME+x}" ] ; then echo "Var \$HOME must be set." ; exit 1 ; fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# $1 Config path $CONFIG_PATH
-# $2 File path $FILE
-create_link () {
-  [[ -f "$1" ]] && rm "$1"
-  ln -s "$2" "$1"
-  echo "$2 -> $1"
+# $1 source folder for links
+# $2 destination folder to create links
+link_recursively () {
+  for DIR in `ls "$1"` ; do
+    FULL_PATH="${1}/$DIR"
+    NEW_PATH="${2}/$DIR"
+    if [[ -f "$FULL_PATH" ]] ; then
+      [[ -f "$NEW_PATH" ]] && rm "$NEW_PATH"
+      ln -s "$FULL_PATH" "$NEW_PATH"
+      echo "$FULL_PATH -> $NEW_PATH"
+    elif [[ -d "$FULL_PATH" ]] ; then 
+      [[ -d "$NEW_PATH" ]] || mkdir "$NEW_PATH"
+      links_in_dir "$FULL_PATH" "$NEW_PATH"
+    fi
+  done
 }
 
 # Link all rc files
@@ -17,13 +26,4 @@ find "${SCRIPT_DIR}" -iname "*rc" | while read FILE; do
   create_link "${HOME}/.$( basename $FILE )" "${FILE}"
 done
 
-# Link all in config folder
-for DIR in ${SCRIPT_DIR}/config/* ; do
-  BASENAME="${HOME}/.config/$( basename $DIR )"
-  [[ -f "${DIR}" ]] && create_link "${BASENAME}" "${DIR}" && continue
-  [[ -d "${DIR}" ]] || continue
-  [[ -d "${BASENAME}" ]] || mkdir "${BASENAME}"
-  for FILE in ${DIR}/* ; do
-    create_link "${BASENAME}/$( basename ${FILE} )" "${FILE}"
-  done
-done
+link_recursively "${SCRIPT_DIR}/config" "${HOME}/.config"
