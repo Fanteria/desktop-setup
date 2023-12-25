4f1b2e0353b4fab0194393bfa607ef1cfd1b0404
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2023-12-25 14:49:39 +0100

    Add todo-tui to workspace 8.

--- config/i3/set_layout.sh
+++ config/i3/set_layout.sh
@@ -4,9 +4,7 @@ sleep 5
 i3-msg "workspace 9: ; append_layout ~/.config/i3/ws.json"
 (kitty btop &)
 sleep 0.5
-(kitty /home/jirka/Projects/gcaltasks/gcaltasks/main.py cal show &)
-sleep 0.5
-(kitty /home/jirka/Projects/gcaltasks/gcaltasks/main.py task show &)
+(kitty todo-tui &)
 sleep 0.5
 (kitty ncspot &)
 
