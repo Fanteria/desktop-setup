7c8d663472b996d3a513b06e5b615dbb682efd78
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2024-07-23 22:26:21 +0200

    Add move workspace to next monitor with focus stay on same monitor.

---
+++ config/hypr/move_workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ACT_MONITOR="$(hyprctl monitors -j | jq '.[] | select(.focused == true) | .id')"
+
+hyprctl dispatch movecurrentworkspacetomonitor +1
+
+hyprctl dispatch focusmonitor "$ACT_MONITOR"
